37980b635feca93f0ba8ae04fdc588e1439a005f
lord-of-the-lost <ignatov.99@yandex.ru>
2025-03-29 00:02:41 +0300

    finish demo

--- DI-Demo/AppDelegate.swift
+++ DI-Demo/AppDelegate.swift
@@ -7,19 +7,82 @@
 
 import UIKit
 
+// MARK: - Demo #1
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
+    let service = Service()
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
+        let viewController = ViewController()
+        viewController.service = service
+        window?.rootViewController = viewController
         window?.makeKeyAndVisible()
         
         return true
     }
 }
 
+// MARK: - Demo #5
+//@main
+//final class AppDelegate: UIResponder, UIApplicationDelegate {
+//    var window: UIWindow?
+//    
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        let viewController = ViewController(
+//            service: Service(),
+//            loggingService: LoggingService(),
+//            networkService: NetworkService(),
+//            analyticsService: AnalyticsService()
+//        )
+//        window?.rootViewController = viewController
+//        window?.makeKeyAndVisible()
+//        
+//        return true
+//    }
+//}
+
+// MARK: - Demo #6
+//@main
+//final class AppDelegate: UIResponder, UIApplicationDelegate {
+//    var window: UIWindow?
+//    
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        registerServices()
+//        
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        let viewController = ViewController()
+//        window?.rootViewController = viewController
+//        window?.makeKeyAndVisible()
+//        
+//        return true
+//    }
+//    
+//    private func registerServices() {
+//        let loggingService = LoggingService()
+//        ServiceLocator.shared.register(service: loggingService) // Сначала регистрируем LoggingService
+//        ServiceLocator.shared.register(service: Service(loggingService: loggingService)) // Затем регистрируем Service
+//        ServiceLocator.shared.register(service: NetworkService())
+//        ServiceLocator.shared.register(service: AnalyticsService())
+//    }
+//}
+
+// MARK: - Demo #7
+//@main
+//final class AppDelegate: UIResponder, UIApplicationDelegate {
+//    var window: UIWindow?
+//    
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        let appComponent = AppComponent()
+//        let viewController = ViewController(dependency: appComponent)
+//        
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.rootViewController = viewController
+//        window?.makeKeyAndVisible()
+//        
+//        return true
+//    }
+//}
--- DI-Demo/ViewController.swift
+++ DI-Demo/ViewController.swift
@@ -7,13 +7,369 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+// MARK: - #1 Property injection
+final class ViewController: UIViewController {
+    var service: Service? // Зависимость через свойство
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        service?.doSomething()
     }
-
-
 }
 
+final class Service {
+    private var isWorkingNow = false
+    
+    func doSomething() {
+        guard !isWorkingNow else { return }
+        isWorkingNow.toggle()
+        
+        print("start work")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            print("end work")
+            self.isWorkingNow.toggle()
+        }
+    }
+}
+
+// MARK: - #2 Method injection
+//final class ViewController: UIViewController {
+//    func configure(with service: Service) { // Зависимость через метод
+//        service.doSomething()
+//    }
+//}
+//
+//final class Service {
+//    private var isWorkingNow = false
+//    
+//    func doSomething() {
+//        guard !isWorkingNow else { return }
+//        isWorkingNow.toggle()
+//        
+//        print("start work")
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//            print("end work")
+//            self.isWorkingNow.toggle()
+//        }
+//    }
+//}
+
+// MARK: - #3 Init injection
+//final class ViewController: UIViewController {
+//    private let service: Service // Зависимость через инициализатор
+//    
+//    init(service: Service) {
+//        self.service = service
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//    
+//    @available(*, unavailable)
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        service.doSomething()
+//    }
+//}
+//
+//final class Service {
+//    private var isWorkingNow = false
+//
+//    func doSomething() {
+//        guard !isWorkingNow else { return }
+//        isWorkingNow.toggle()
+//
+//        print("start work")
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//            print("end work")
+//            self.isWorkingNow.toggle()
+//        }
+//    }
+//}
+
+// MARK: - #4 DI in MVP
+//final class UserPresenter {
+//    private weak var view: UserViewController?
+//    
+//    func setupView(_ view: UserViewController) {
+//        self.view = view
+//    }
+//    
+//    func loadUsers() {
+//        let users = ["Alice", "Bob", "Charlie"]
+//        view?.displayUsers(users)
+//    }
+//}
+//
+//final class UserViewController: UIViewController {
+//    private let presenter: UserPresenter
+//    
+//    init(presenter: UserPresenter) {
+//        self.presenter = presenter
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//    
+//    @available(*, unavailable)
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        presenter.setupView(self)
+//        presenter.loadUsers()
+//    }
+//    
+//    func displayUsers(_ users: [String]) {
+//        print("Users: \(users)")
+//    }
+//}
+//
+///// Глобальный метод имитирующий фабрику для сборки модуля и примера DI
+//func crateUserModule() -> UserViewController {
+//    let presenter = UserPresenter()
+//    let controller = UserViewController(presenter: presenter)
+//    presenter.setupView(controller)
+//    return controller
+//}
+//
+
+
+// MARK: - #5 Init injection issue
+//final class ViewController: UIViewController {
+//    private let service: Service
+//    private let loggingService: LoggingService
+//    private let networkService: NetworkService
+//    private let analyticsService: AnalyticsService
+//    
+//    init(service: Service, loggingService: LoggingService, networkService: NetworkService, analyticsService: AnalyticsService) {
+//        self.service = service
+//        self.loggingService = loggingService
+//        self.networkService = networkService
+//        self.analyticsService = analyticsService
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//    
+//    @available(*, unavailable)
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        service.doSomething()
+//        loggingService.log(message: "View did load")
+//        networkService.fetchData()
+//        analyticsService.trackEvent(event: "View Loaded")
+//    }
+//}
+//
+//// MARK: - Моковые сервисы
+//final class Service {
+//    private var isWorkingNow = false
+//    
+//    func doSomething() {
+//        guard !isWorkingNow else { return }
+//        isWorkingNow.toggle()
+//        
+//        print("start work")
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//            print("end work")
+//            self.isWorkingNow.toggle()
+//        }
+//    }
+//}
+//
+//
+//final class LoggingService {
+//    func log(message: String) {
+//        print("Log: \(message)")
+//    }
+//}
+//
+//final class NetworkService {
+//    func fetchData() {
+//        print("Fetching data...")
+//    }
+//}
+//
+//final class AnalyticsService {
+//    func trackEvent(event: String) {
+//        print("Tracking event: \(event)")
+//    }
+//}
+//
+
+// MARK: - #6 ServiceLocator for DI
+//final class ServiceLocator {
+//    static let shared = ServiceLocator()
+//    private var services: [String: Any] = [:]
+//    
+//    private init() {}
+//    
+//    func register<T>(service: T) {
+//        let key = String(describing: T.self)
+//        services[key] = service
+//    }
+//    
+//    func resolve<T>() -> T? {
+//        let key = String(describing: T.self)
+//        return services[key] as? T
+//    }
+//}
+//
+//final class ViewController: UIViewController {
+//    private let service: Service
+//    private let loggingService: LoggingService
+//    private let networkService: NetworkService
+//    private let analyticsService: AnalyticsService
+//    
+//    init() {
+//        // Получаем зависимости из сервис-локатора
+//        guard
+//            let service: Service = ServiceLocator.shared.resolve(),
+//            let loggingService: LoggingService = ServiceLocator.shared.resolve(),
+//            let networkService: NetworkService = ServiceLocator.shared.resolve(),
+//            let analyticsService: AnalyticsService = ServiceLocator.shared.resolve()
+//        else {
+//            fatalError("Failed to resolve dependencies")
+//        }
+//        
+//        self.service = service
+//        self.loggingService = loggingService
+//        self.networkService = networkService
+//        self.analyticsService = analyticsService
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//    
+//    @available(*, unavailable)
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        service.doSomething()
+//        loggingService.log(message: "View did load")
+//        networkService.fetchData()
+//        analyticsService.trackEvent(event: "View Loaded")
+//    }
+//}
+//
+//// MARK: - Моковые сервисы
+//final class Service {
+//    private var isWorkingNow = false
+//    private let loggingService: LoggingService
+//    
+//    init(loggingService: LoggingService) {
+//        self.loggingService = loggingService
+//    }
+//    
+//    func doSomething() {
+//        guard !isWorkingNow else { return }
+//        isWorkingNow.toggle()
+//        
+//        loggingService.log(message:"start work")
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//            self.loggingService.log(message:"end work")
+//            self.isWorkingNow.toggle()
+//        }
+//    }
+//}
+//
+//
+//final class LoggingService {
+//    func log(message: String) {
+//        print("Log: \(message)")
+//    }
+//}
+//
+//final class NetworkService {
+//    func fetchData() {
+//        print("Fetching data...")
+//    }
+//}
+//
+//final class AnalyticsService {
+//    func trackEvent(event: String) {
+//        print("Tracking event: \(event)")
+//    }
+//}
+//
+
+// MARK: - #7 AppComponent for DI
+//final class AppComponent {
+//    lazy var loggingService: LoggingService = LoggingService()
+//    lazy var service: Service = Service(loggingService: loggingService)
+//    lazy var networkService: NetworkService = NetworkService()
+//    lazy var analyticsService: AnalyticsService = AnalyticsService()
+//}
+//
+//final class ViewController: UIViewController {
+//    private let service: Service
+//    private let loggingService: LoggingService
+//    private let networkService: NetworkService
+//    private let analyticsService: AnalyticsService
+//    
+//    init(dependency: AppComponent) {
+//        self.service = dependency.service
+//        self.loggingService = dependency.loggingService
+//        self.networkService = dependency.networkService
+//        self.analyticsService = dependency.analyticsService
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//    
+//    @available(*, unavailable)
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        service.doSomething()
+//        loggingService.log(message: "View did load")
+//        networkService.fetchData()
+//        analyticsService.trackEvent(event: "View Loaded")
+//    }
+//}
+//
+//final class Service {
+//    private var isWorkingNow = false
+//    private let loggingService: LoggingService
+//    
+//    init(loggingService: LoggingService) {
+//        self.loggingService = loggingService
+//    }
+//    
+//    func doSomething() {
+//        guard !isWorkingNow else { return }
+//        isWorkingNow.toggle()
+//        
+//        loggingService.log(message:"start work")
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//            self.loggingService.log(message:"end work")
+//            self.isWorkingNow.toggle()
+//        }
+//    }
+//}
+//
+//final class LoggingService {
+//    func log(message: String) {
+//        print("Log: \(message)")
+//    }
+//}
+//
+//final class NetworkService {
+//    func fetchData() {
+//        print("Fetching data...")
+//    }
+//}
+//
+//final class AnalyticsService {
+//    func trackEvent(event: String) {
+//        print("Tracking event: \(event)")
+//    }
+//}
+//
